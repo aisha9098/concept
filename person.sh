e2a637624f6db91c62a54d9a4c9fff5bc316647e
Aisha <shaik.aisha9955@gmail.com>
2024-08-07 22:03:41 +0530

    new file

---
+++ person.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+# assigning vale to the variable 
+
+# person1=naga
+#person2=aisha
+echo "hi $person2 how are you  "
+echo "hi $person1 am fine "
+echo " i love you $person1"
+echo "i too love you $person2"
