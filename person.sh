bc86384255f7ef98498b59233d94504c2d5816bd
Aisha <shaik.aisha9955@gmail.com>
2024-08-07 22:04:40 +0530

    new file

--- person.sh
+++ person.sh
@@ -1,8 +1,8 @@
 !/bin/bash
 # assigning vale to the variable 
 
-# person1=naga
-#person2=aisha
+person1=naga
+person2=aisha
 echo "hi $person2 how are you  "
 echo "hi $person1 am fine "
 echo " i love you $person1"
