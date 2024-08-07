55917c8fd0913c207f5e498f9d0f48801374f9f7
Aisha <shaik.aisha9955@gmail.com>
2024-08-07 21:50:49 +0530

    new file

--- variable.sh
+++ variable.sh
@@ -2,5 +2,5 @@
 # assigning vale to the variable 
 
 # shell script run the command in side &() and stores out put  called DATE
-DATE=&(date)
-echo " hello and the time stamp is :&DATE"
+DATE=$(date)
+echo " hello and the time stamp is :$DATE"
